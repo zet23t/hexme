eda1bbe9b201735c6d701c18f4ef2bfd2a2522df
Eike Decker <zet23t@googlemail.com>
2026-07-07 16:41:02 +0200

    moving to trihexes

--- src/assets.c
+++ src/assets.c
@@ -2,24 +2,38 @@
 
 game_assets_t g_assets;
 
+
 Model load_model(const char *path)
 {
-    Model m = LoadModel(path);
+    Model m = LoadModel(TextFormat("resources/%s.glb",path));
     m.materials[0].maps[MATERIAL_MAP_ALBEDO].texture = g_assets.modelAtlas;
     return m;
+}
+
+tri_hex_t load_tri_hex(const char *name, map_type_e center, map_type_e a, map_type_e b)
+{
+    Model m = load_model(name);
+    return (tri_hex_t){
+        .model = m, .center = center, .corner_a = a, .corner_b = b
+    };
 }
 
 void assets_load(void)
 {
     g_assets.modelAtlas = LoadTexture("resources/atlas.png");
     SetTextureFilter(g_assets.modelAtlas, TEXTURE_FILTER_BILINEAR);
-    g_assets.hex_grass = load_model("resources/h_grass.glb");
-    g_assets.hex_water = load_model("resources/h_water.glb");
-    g_assets.pawns[0] = load_model("resources/pawn.glb");
-    g_assets.pawns[1] = load_model("resources/pawn-2.glb");
-    g_assets.pawn_shadow = load_model("resources/pawn-shadow.glb");
-    g_assets.confirs[0] = load_model("resources/prop_confir-1.glb");
-    g_assets.confirs[1] = load_model("resources/prop_confir-2.glb");
-    g_assets.confirs[2] = load_model("resources/prop_confir-3.glb");
+    g_assets.hex_grass = load_model("h_grass");
+    g_assets.hex_water = load_model("h_water");
+    g_assets.pawns[0] = load_model("pawn");
+    g_assets.pawns[1] = load_model("pawn-2");
+    g_assets.pawn_shadow = load_model("pawn-shadow");
+    g_assets.confirs[0] = load_model("prop_confir-1");
+    g_assets.confirs[1] = load_model("prop_confir-2");
+    g_assets.confirs[2] = load_model("prop_confir-3");
+
+    int tcnt = 0;
+    g_assets.tri_hexes[tcnt++] = load_tri_hex("t_grass_mud_001", MAP_TYPE_GRASS, MAP_TYPE_GRASS, MAP_TYPE_MUD);
+    g_assets.tri_hexes[tcnt++] = load_tri_hex("t_grass_mud_010", MAP_TYPE_GRASS, MAP_TYPE_MUD, MAP_TYPE_GRASS);
+    g_assets.tri_hexes[tcnt++] = load_tri_hex("t_grass_mud_011", MAP_TYPE_GRASS, MAP_TYPE_MUD, MAP_TYPE_MUD);
     
 }
--- src/assets.h
+++ src/assets.h
@@ -3,15 +3,26 @@
 
 #include <raylib.h>
 
+typedef enum {
+    MAP_TYPE_GRASS,
+    MAP_TYPE_WATER,
+    MAP_TYPE_MUD,
+} map_type_e;
 
+typedef struct {
+    Model model;
+    map_type_e center, corner_a, corner_b;
+} tri_hex_t;
 
 typedef struct {
     Texture2D modelAtlas;
     Model hex_grass;    
     Model hex_water;    
-    Model pawns[2];    
+    Model pawns[2];
     Model pawn_shadow;    
     Model confirs[3];    
+
+    tri_hex_t tri_hexes[8];
 
 } game_assets_t;
 
